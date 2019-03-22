be5597a20bfbf5884080e547f3f16e7fa2a4a6dc
nathalyramirez <nathalyramirez@NDRONATHALYRAMI.sml.com>
2019-03-22 17:22:36 -0400

    problem 4 resolved

--- src/Problem4/Cannonball.java
+++ src/Problem4/Cannonball.java
@@ -2,18 +2,33 @@ package Problem4;
 
 public class Cannonball {
 
-	private int height = 0;
-
 	public Cannonball() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Cannonball(int h) {
-		super();
-		this.height = h;
+	
+	public int howManyCannonballsAre (int height) 
+	{
+		int result = 0;
+		if (height == 1)
+		{
+			return result = result + 1;
+		}
+		else 
+		{
+			return result = result + (height * height) + howManyCannonballsAre( (height-1) ); 	
+		}
 	}
 	
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Cannonball cannonball = new Cannonball();
+		System.out.println("The Pyramid has: " + cannonball.howManyCannonballsAre(7) + " Cannonballs");
+		
+
+	}
 	
 }
